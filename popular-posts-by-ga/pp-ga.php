adc13beb1a343e2697c37b138f37f75b2a11200a
Jake Tolbert <crazybilly@gmail.com>
2009-10-27 01:23:17 -0500

    worked on plugin - has problems

--- popular-posts-by-ga/pp-ga.php
+++ popular-posts-by-ga/pp-ga.php
@@ -12,7 +12,7 @@ Description: a sidebar widget that displays your most popular posts based on Goo
 if (!class_exists("pp_ga")) {
 	class pp_ga {
 		function pp_ga () { //constructor
-		}
+		
 
 		// a sample action
 		function addHeaderCode () {
@@ -56,6 +56,8 @@ if (!class_exists("pp_ga")) {
 		}
 
 
+
+
 	/*----------------PRINT THE ADMIN PAGE ----------------------*/
 
 		function printAdminPage () {
@@ -65,11 +67,11 @@ if (!class_exists("pp_ga")) {
 
 			//look for user input and update if necessary
 			if (isset($_POST['update_pp_ga_Settings'])) {
-				if (isset $_POST['pp_ga_Header'])) {
+				if (isset ($_POST['pp_ga_Header'])) {
 					$pp_ga_Options['show_header'] = $_POST['pp_ga_Header'];
 				}
 
-				if (isset $_POST['pp_ga_AddContent'])) {
+				if (isset ($_POST['pp_ga_AddContent'])) {
 					$pp_ga_Options['add_content'] = $_POST['pp_ga_AddContent'];
 				}
 
@@ -78,7 +80,7 @@ if (!class_exists("pp_ga")) {
 				}
 
 				if (isset ($_POST['pp_ga_Content'])) {
-					$pp_ga_Options['content'] = apply_filters('content_save_pre', $_POST['pp_ga_Content'];
+					$pp_ga_Options['content'] = apply_filters('content_save_pre', $_POST['pp_ga_Content']);
 				}
 
 				//save to db
@@ -97,24 +99,132 @@ if (!class_exists("pp_ga")) {
 		
 			//Create the admin panel
 			?>
+			
+			<div class="wrap">
+				<form method="post" action"<?php echo $_SERVER["REQUEST_URI"]; ?>">
+				<h2>Popular Posts by Google Analytics</h2>
 
-			<!-- am working from here: http://www.devlounge.net/articles/constructing-an-wordpress-plugin-admin-panel
+				<h3>Content to Add to the end of the post</h3>
 
-			on the section that's all html --><?
+					<textarea name="pp_ga_Content" style="width: 80%; height: 100px;">
+						<?php _e(apply_filters('format_to_edit',$pp_ga_Options['content']),'pp_ga') ?>
+					</textarea>
+
+				<h3>Allow Comment Code in the Header?</h3>
+
+					<p>Selecting "No" will disable the comment code inserted in the header</p>
+					<p><label for="pp_ga_Header_yes">
+						<input type="radio" id="pp_ga_Header_yes" name="pp_ga_Header" value="true"
+							<?php if ($pp_ga_Options['show_header'] == 'true') {
+									_e('checked="checked"','pp_ga'); 
+									}
+								?> />
+							Yes
+						</label>
+						
+						&nbsp;&nbsp;&nbsp;&nbsp;
+						
+						<label for "pp_ga_Header_no">
+							<input type="radio" id="pp_ga_Header_no" name="pp_ga_Header" value="false"
+								<?php if ($pp_ga_Options['Header'] == "false") {
+									_e('checked="checked"','pp_ga');
+									}
+								?> />
+							No
+						</label>
+					</p>
+
+				<h3>Allow Content Added to the End of a Post?</h3>
+
+				<p>Selecting "No" will disable the comment from being added in the post end</p>
+
+		
+				<p><label for="pp_ga_AddContent_yes">
+					<input type="radio" id="pp_ga_AddContent_yes" name="pp_ga_AddContent" value="true" 
+						<?php if ($pp_ga_Options['add_content'] == "true") { 
+							_e('checked="checked"', "pp_ga"); }
+								?> /> 
+						Yes
+					</label>
+					
+					&nbsp;&nbsp;&nbsp;&nbsp;
+					
+					<label for="pp_ga_AddContent_no">
+						<input type="radio" id="pp_ga_AddContent_no" name="pp_ga_AddContent" value="false" 
+							<?php if ($pp_ga_Options['add_content'] == "false") { 
+								_e('checked="checked"', "pp_ga");
+								}?>/> 
+							No
+					</label>
+				</p>
+
+				<h3>Allow Comment Authors to be Uppercase?</h3>
+
+				<p>Selecting "No" will leave the comment authors alone.</p>
+
+				<p><label for="pp_ga_Author_yes">
+					<input type="radio" id="pp_ga_Author_yes" name="pp_ga_Author" value="true" 
+						<?php if ($pp_ga_Options['comment_author'] == "true") { 
+							_e('checked="checked"', "pp_ga"); 
+							}?> /> 
+						Yes
+					</label>
+					
+					&nbsp;&nbsp;&nbsp;&nbsp;
+					
+					<label for="pp_ga_Author_no">
+						<input type="radio" id="pp_ga_Author_no" name="devloungeAuthor" value="false" 
+							<?php if ($pp_ga_Options['comment_author'] == "false") { 
+								_e('checked="checked"', "pp_ga_PluginSeries"); 
+								}?>/> 
+						No
+					</label>
+				</p>
+
+				<div class="submit">
+					<input type="submit" name="update_pp_ga_Settings" value="
+						<? _e('Update Settings', 'pp_ga') ?>" />
+				</div>
+
+			</form>
+		</div>
+			<?
+	} // admin function
+
+
+
+	}} //end class definition
+} //end checking for class_exists 
 
 
 
 
 
-}
+
 
 if (class_exists("pp_ga")) {
-	$dl_pluginSeries = new pp_ga();
+	$dl_pp_ga = new pp_ga();
+	}
+
+
+//Initalize the admin panel
+if (!function_exists("pp_ga_ap")) {
+	function pp_ga_ap () {
+		global $dl_pp_ga;
+
+		if (!isset($dl_pp_ga)) {
+			return;
+			}
+
+		if (function_exists('add_options_page')) {
+			add_options_page('Popular Posts by Google Analytics','Popular Posts by GA',9,basename(_FILE_),array(&$dl_pp_ga,'printAdminPage'));
+			}
+		}
 	}
 
 //Actions and Filters
 
-if (isset($dl_pluginSeries)) {
+if (isset($dl_pp_ga)) {
 	
 	//Actions - use these to ADD code
 
@@ -122,7 +232,10 @@ if (isset($dl_pluginSeries)) {
 		add_action('wp_head', array(&$pp_ga, 'addHeaderCode'),1);
 
 		//get the default values when you turn the plugin on
-		add_action('popular-posts-by-ga/pp-ga.php', array (&$dl_pluginSeries, 'init'));
+		add_action('popular-posts-by-ga/pp-ga.php', array (&$dl_pp_ga, 'init'));
+
+		//call the function to write the admin page
+		add_action('admin_menu','pp_ga_ap');
 		
 	//Filters - use these to modify code
 
